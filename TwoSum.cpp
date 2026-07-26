6b53ff70c334b672630859ea5c91a88650dd3fa2
parthvats-code <parthvats184@gmail.com>
2026-07-27 00:43:31 +0530

    added working sol

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,23 +1,40 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) { 
-           unordered_map<int, int> mp;
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
-        for(int i = 0; i < nums.size(); i++)
+using namespace std;
+
+class TwoSum
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++)
         {
             int need = target - nums[i];
 
-            if(mp.find(need) != mp.end())
+            if (mp.find(need) != mp.end())
             {
-                return {mp[need], i};
+                return {mp[need], i}; // Requires C++11 or higher
             }
 
             mp[nums[i]] = i;
         }
 
         return {};
-    };
-        
-    
-
+    } // Removed extra semicolon
 };
+
+// Driver code to make VS Code happy
+int main()
+{
+    TwoSum solution;
+    int n, target;
+
+    cout << "Enter target number: ";
+    cin >> target;
+
+    // ... rest of your code
+}
